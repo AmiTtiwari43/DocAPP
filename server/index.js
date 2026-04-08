559b88af9333635888809fb76b86b92b75aa568b
AMIT TIWARI <149927564+AmiTtiwari43@users.noreply.github.com>
2026-04-09 00:30:33 +0530

    Comment out Cross-Origin-Embedder-Policy header
    Comment out the Cross-Origin-Embedder-Policy header for Google Sign-In.

--- server/index.js
+++ server/index.js
@@ -51,7 +51,7 @@ app.use(
 // Add headers for Google Sign-In (COOP/COEP)
 app.use((req, res, next) => {
   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
-  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  //res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
   next();
 });
 
